1e0c31cd9fedb3dc6301113502cfb121fdaba798
Christopher Fong <55607360+cfong2@users.noreply.github.com>
2022-02-28 20:15:30 -0500

    Add files via upload
    page files

---
+++ highlights.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        pause: 2500,
+        randomStart: true,
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1,
+        captions: true,
+        slideMargin: 20
+    });
+});
